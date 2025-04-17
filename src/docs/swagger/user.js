98f90e3a08b207cd08b04754fbea8e7335b47d68
nguyenmanh1808 <nguyenducmanhtm12.com>
2025-04-17 14:55:53 +0700

    fix count task

--- src/docs/swagger/user.js
+++ src/docs/swagger/user.js
@@ -48,7 +48,7 @@ const userSwagger = {
       },
     },
   },
-  "/user/update-profile": {
+  "/users/update-profile": {
     put: {
       tags: ["User"],
       summary: "Cập nhật thông tin người dùng",
--- src/users/user.route.js
+++ src/users/user.route.js
@@ -3,15 +3,16 @@ const  userController = require("./user.controller.js");
 const upload = require("../config/multer.js");
 const routeUser = express.Router();
 
-
 routeUser.route("/")
-    .get(userController.getAllUsers)
+    .get(userController.getAllUsers);
+routeUser.put("/update-profile", upload.single("avatar"), userController.updateUser);
 routeUser.route("/:id")
     .get(userController.getUserById)
     .put(userController.updateUser)
 routeUser.param("id", userController.load);
 
-routeUser.put("/update-profile", upload.single("avatar"), userController.updateUser);
+
+
 
 
 module.exports = routeUser;
