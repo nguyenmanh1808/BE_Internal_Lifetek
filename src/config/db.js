6aec6439ad61c0404f60ceb7b8964c762a5f2ba1
tuantrandever <tuant4112@gmail.com>
2025-04-25 16:09:05 +0700

    chốt phiên bản hiện tại

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,24 @@
 const mongoose = require("mongoose");
-Promise = require('bluebird');
+Promise = require("bluebird");
 const env = require("./env");
-
 
 mongoose.Promise = Promise;
 const connectDB = async () => {
   // console.log(env.MONGO_HOST);
 
   try {
-    if (env.NODE_ENV === "development") {
+    if (env.NODE_ENV === "development1") {
       // mongoose.set("debug", true);
       await mongoose.connect(env.URI_DB);
       console.log("✅ Kết nối MongoDB Atlas cá nhân thành công!");
-
     } else {
       await mongoose.connect(env.MONGO_HOST, {
         user: env.MONGO_USERNAME,
         pass: env.MONGO_PASSWORD,
         serverSelectionTimeoutMS: 5000,
       });
-    console.log("✅ Kết nối MongoDB Atlas company thành công!");
+      console.log("✅ Kết nối MongoDB Atlas company thành công!");
     }
-
   } catch (error) {
     console.log("❌ Lỗi kết nối MongoDB:", error);
     process.exit(1);
