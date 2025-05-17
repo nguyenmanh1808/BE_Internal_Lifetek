5d7e21b722a992cc060c5e10e58aa366d64e1160
nguyenmanh1808 <nguyenducmanhtm12.com>
2025-05-17 21:28:20 +0700

    work flow

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const projectRoleRoutes = require("../projectRole/projectRole.route.js");
 const workFlow = require("../workflow/workFlow.route.js")
 const router = express.Router();
 
-router.use("/auth", routerAuth);
+// router.use("/auth", routerAuth);
 
 const routes = {
     "/projects": routerProject,
@@ -23,7 +23,7 @@ const routes = {
 };
 
 Object.entries(routes).forEach(([path, rou]) => {    
-    router.use(path, authMiddleware ,rou);
+    router.use(path/*, authMiddleware */,rou);
 });
 
 module.exports = router;
--- src/workflow/workFlow.service.js
+++ src/workflow/workFlow.service.js
@@ -13,8 +13,9 @@ exports.createWorkflow = async (data) => {
     projectId: data.projectId
   };
 
-  const existing = await workFlow.find({ projectId:data.projectId });
-  if (existing) throw new Error('Code workflow đã tồn tại');
+  const existing = await workFlow.find({ projectId: data.projectId });
+  console.log(existing);
+  if (existing.length != 0) throw new Error('Code workflow đã tồn tại');
   const workflow = await workFlow.create(code);
   return workflow;
 }
