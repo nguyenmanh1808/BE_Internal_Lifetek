f1119dd950736140e7404df4eddaabd00916add0
tuantrandever <tuant4112@gmail.com>
2025-05-17 22:37:31 +0700

    lại

--- src/workflow/workFlow.service.js
+++ src/workflow/workFlow.service.js
@@ -13,7 +13,7 @@ exports.createWorkflow = async (data) => {
   };
 
   const existing = await workFlow.find({ projectId: data.projectId });
-  if (existing) throw new Error("Code workflow đã tồn tại");
+  if (existing.length != 0) throw new Error("Code workflow đã tồn tại");
   const workflow = await workFlow.create(code);
   return workflow;
 };
--- src/workflow/workflow.model.js
+++ src/workflow/workflow.model.js
@@ -1,11 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workflowSchema = new Schema({
-  name: { type: String, required: false },              // Tên hiển thị của workflow
-  createdAt: { type: Date, default: Date.now },        // Thời gian tạo
-  projectmanager: { type: Schema.Types.ObjectId, ref: 'User' }, // Ai tạo (nếu cần phân quyền cao hơn)
-  projectId: { type: Schema.Types.ObjectId, ref: 'Project' }
+  name: { type: String, required: false }, // Tên hiển thị của workflow
+  createdAt: { type: Date, default: Date.now },
+  code: {
+    type: String,
+    required: true,
+    unique: true,
+  }, // Thời gian tạo
+  projectmanager: { type: Schema.Types.ObjectId, ref: "User" }, // Ai tạo (nếu cần phân quyền cao hơn)
+  projectId: { type: Schema.Types.ObjectId, ref: "Project" },
 });
 
-module.exports = mongoose.model('Workflow', workflowSchema);
+module.exports = mongoose.model("Workflow", workflowSchema);
