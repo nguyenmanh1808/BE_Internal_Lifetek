fe49198cebc6a59c3ae90afbf7246d8582ce0746
nguyenmanh1808 <nguyenducmanhtm12.com>
2025-05-22 10:15:38 +0700

    a

--- src/workflow/workflow.model.js
+++ src/workflow/workflow.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const workflowSchema = new Schema({          // Tên hiển thị của workflow
+const workflowSchema = new Schema({          
   createdAt: { type: Date, default: Date.now },        // Thời gian tạo
   projectmanager: { type: Schema.Types.ObjectId, ref: 'User' }, // Ai tạo (nếu cần phân quyền cao hơn)
   projectId: { type: Schema.Types.ObjectId, ref: 'Project' }
